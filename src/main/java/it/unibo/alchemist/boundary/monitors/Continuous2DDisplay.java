16b37ca8666b022f825aa2b4e98a891df1701fc4
Danilo Pianini <danilo.pianini@unibo.it>
2015-11-12 17:41:20 +0100

    Deprecate now useless Continuous2DDisplay

--- src/main/java/it/unibo/alchemist/boundary/monitors/Continuous2DDisplay.java
+++ src/main/java/it/unibo/alchemist/boundary/monitors/Continuous2DDisplay.java
@@ -14,6 +14,7 @@ package it.unibo.alchemist.boundary.monitors;
  * @param <T>
  *            Concentration
  */
+@Deprecated
 public class Continuous2DDisplay<T> extends Generic2DDisplay<T> {
 
 	private static final long serialVersionUID = -8639779509640696726L;
